25b92d87b801f89a9900abaaedb6a1648f3cca8e
Vitor Santos <vitor.asantos@b2wdigital.com>
2019-08-20 18:15:11 -0300

    Feat: Fetch document from Mongo Database

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [
+                 [org.clojure/clojure "1.10.0"]
+                 [com.novemberain/monger "3.1.0"]
+                 [io.forward/yaml "1.0.9"]
+  ]
   :main ^:skip-aot json-to-yaml.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/json_to_yaml/core.clj
+++ src/json_to_yaml/core.clj
@@ -1,7 +1,44 @@
 (ns json-to-yaml.core
-  (:gen-class))
+	(:gen-class)
+	(:require [monger.core])
+	(:require [monger.collection])
+	(:import org.bson.types.ObjectId)
+ )
+
+(def mongo-database-id "test_database")
+(def mongo-collection-id "test_collection")
+(def mongo-connection-host "localhost")
+(def mongo-connection-port 27017)
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+	"Convert Mongo Database JSON to a YAML file"
+	[document-id]
+	(println (str "DEBUG: Executing program with params [" document-id "]"))
+	(let [
+		mongo-connection (monger.core/connect {:host mongo-connection-host :port mongo-connection-port})
+		mongo-database (monger.core/get-db mongo-connection mongo-database-id)
+	]
+		(def mongo-document
+			(monger.collection/find-maps
+				mongo-database
+				mongo-collection-id
+				{
+					:_id (ObjectId. document-id)
+				}
+			)
+		)
+
+		(println (str
+			"DEBUG: "
+			"Searching for document with id "
+			document-id
+			" in "
+			mongo-database-id "@" mongo-connection-host ":" mongo-connection-port "/" mongo-collection-id
+		))
+
+		(println "DEBUG: Search result:")
+		(println mongo-document)
+
+		(monger.core/disconnect mongo-connection)
+	)
+)
